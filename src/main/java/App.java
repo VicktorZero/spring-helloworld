8c7f5f11623ea13ba3279533e901e487e955c56f
Victor Glyzin <victozer@icloud.com>
2021-12-23 21:59:57 +0300

    Commit1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println("__________________________________________");
+        System.out.println(bean==bean2);
+        System.out.println("___________________________________________");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat==cat2);
     }
 }
